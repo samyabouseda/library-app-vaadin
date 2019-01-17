86006bdc0fa1f327b56e56f23e25be0e56573d33
samyabouseda <samy@abouseda.ch>
2019-01-17 21:21:57 +0100

    Add styling to home page.

--- src/main/java/ch/hesge/vaadin/ui/views/Home.java
+++ src/main/java/ch/hesge/vaadin/ui/views/Home.java
@@ -2,7 +2,6 @@ package ch.hesge.vaadin.ui.views;
 
 
 import ch.hesge.vaadin.ui.common.components.NavBar;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -13,23 +12,36 @@ import com.vaadin.flow.router.Route;
 @Route("")
 public class Home extends VerticalLayout {
 
+    private NavBar navBar = new NavBar();
+    private TextArea textArea = new TextArea();
+    private Image image = new Image();
+    private HorizontalLayout pageContent = new HorizontalLayout();
+
     public Home() {
         initView();
+        addStyling();
     }
 
     private void initView() {
-        NavBar navBar = new NavBar();
+        textArea.setValue("Bienvenue dans votre biblioteque virtuelle \n \n Sed ut perspiciatis unde omnis " +
+                "iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa" +
+                " quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim" +
+                " ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni " +
+                "dolores eos qui ratione voluptatem sequi nesciunt. ");
 
-        TextArea area1 = new TextArea();
-        area1.setValue("Bienvenue dans votre biblioteque virtuelle \n \n Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ");
+        image.setSrc("http://bit.ly/2Fzlnpd");
 
-        Image image = new Image();
-        image.setSrc("https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Ftimedotcom.files.wordpress.com%2F2015%2F06%2F521811839-copy.jpg&w=1600&c=sc&poi=face&q=70");
-
-        HorizontalLayout pageContent = new HorizontalLayout();
-        pageContent.setWidth("100%");
-        pageContent.add(area1, image);
-
+        pageContent.add(textArea, image);
         add(navBar, pageContent);
     }
+
+    private void addStyling() {
+        pageContent.getStyle().set("width", "100%");
+        pageContent.getStyle().set("max-height", "80vh");
+        textArea.getStyle().set("padding", "0px");
+        textArea.getStyle().set("width", "30%");
+        image.getStyle().set("width", "70%");
+    }
+
+
 }
